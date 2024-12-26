2ed72307c21c59d8bf3503c33328e2e294afa40f
hjs <legoesprit@kabelbw.de>
2024-12-26 10:55:12 +0100

    Initial Commit

---
+++ TestOpenAliasTests/TestOpenAliasTests.swift
@@ -0,0 +1,17 @@
+//
+//  TestOpenAliasTests.swift
+//  TestOpenAliasTests
+//
+//  Created by Heinz-Jörg on 26.12.24.
+//
+
+import Testing
+@testable import TestOpenAlias
+
+struct TestOpenAliasTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
